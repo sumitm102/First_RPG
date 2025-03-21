8a6351569b7b760583cf540ae95ce65f56eccbf2
sumitm102 <sumitmandal0102@gmail.com>
2025-03-21 03:32:58 -0400

    Added attack collision detection and hit fx

--- Assets/Scripts/Player/PlayerStates/PlayerGroundedState.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerGroundedState.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class PlayerGroundedState : PlayerState {
+
     public PlayerGroundedState(Player _player, PlayerStateMachine _playerStateMachine, string _animBoolName) : base(_player, _playerStateMachine, _animBoolName) {
     }
 
@@ -11,16 +12,16 @@ public class PlayerGroundedState : PlayerState {
     public override void UpdateState() {
         base.UpdateState();
 
+        //If space key pressed and player is grounded, change to jump state
+        if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected())
+            playerStateMachine.ChangeState(player.playerJumpState);
+
         if (Input.GetMouseButtonDown(0))
             playerStateMachine.ChangeState(player.playerPrimaryAttackState);
 
         //To make sure fall animation plays and not anything else, ie: after dashing during jump
         if (!player.IsGroundDetected())
             playerStateMachine.ChangeState(player.playerAirState);
-
-        //If space key pressed and player is grounded, change to jump state
-        if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected()) 
-            playerStateMachine.ChangeState(player.playerJumpState);
 
     }
 
